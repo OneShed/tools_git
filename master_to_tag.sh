586dd12f29ab40ebf2106e2a370dbc71fb9b468d
vobadm <cfm@clearstream.com>
2017-10-02 16:12:50 +0200

    Fixed: Push would fail if repo specified via global path

--- master_to_tag.sh
+++ master_to_tag.sh
@@ -3,6 +3,7 @@
 # create (move) master to tag $1 and push it to rel repo for single repo $REPO
 # e.g $0 CBLR1640 cs.rdf
 # exit if master is on "${TAG}_PREPROD"
+# e.g $0 master_to_tag.sh CBLS1750_TI_RHEL7_PREPROD /local/git/repos/CBLS1750_TI_RHEL7_PREPROD/cs.rts
 
 set -e
 set -u
@@ -13,7 +14,7 @@ TAG_PREPROD="${TAG}_PREPROD"
 REL=https://github.deutsche-boerse.de/rel
 
 if [[ ! -d ${repo} ]]; then
-	echo "Repo ${repo} does not exits"
+	echo "Repo ${repo} does not exist"
 	exit 1
 fi
 
@@ -27,7 +28,7 @@ exit_if_preprod() {
 
 		# master is on PREPROD => return 1
 		if [[ "${rev_preprod}" == "${rev_master}" ]]; then
-			echo "$(basename $0): $TAG_PREPROD exists => nothing done."
+			echo "$(basename $0): $TAG_PREPROD exissts => nothing done."
 			exit 1
 		fi
 	fi
@@ -38,10 +39,12 @@ if [[ ! "${TAG}" == *PREPROD ]]; then
 	exit_if_preprod
 fi
 
+REPO_NAME="${repo##*\/}"
+
 git checkout ${TAG}
 git branch -f master
 echo 'Pushing master to scm_repo'
-git push -f $REL/$repo master
+git push -f $REL/$REPO_NAME master
 
 # Set sync status against master 
 set_sync.pl --repo "${PWD}" --verbose --tag_impl "${TAG}"
