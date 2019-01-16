bf4aba626442e0aa4ffc730f548f3b58ed5406f5
vobadm <cfm@clearstream.com>
2019-01-16 12:37:02 +0100

    modified:   create_fork_repo.sh

--- create_fork_repo.sh
+++ create_fork_repo.sh
@@ -3,7 +3,7 @@
 # Usage: $0 [<repo_name>]
 # Creates a new repo in rel org and forks it to dev
 #
-# Example: $0 cs.ddr_ddl description topic cs-scm-luxembourg,cs-scm-frankfurt wg399,hm410
+# Example: $0 cs.ddr_ddl description topic cs-cfm-community,admin,,cs-scm-frankfurt,write wg399,hm410
 #
 # $github_token must be exported (e.g. put it to .bashrc)
 
@@ -17,49 +17,85 @@ GITHUB_URL=https://github.deutsche-boerse.de
 # mandatory
 repo="${1}"
 description=${2}
-topic=${3}
-teams=${4} #teamname,admin,,teamname2,write
-collaborators=${5}
+topics=${3,,}
+# optional
+teams=${4:-} #teamname,admin,,teamname2,write
+collaborators_dev=${5:-}
+# TODO collaborators_rel=${6:-}
 team_id_rel=103 # SCM Luxembourg
 
 token="${SCMLUXADM_TOKEN}"
 
-# create repo in rel, add '-v' for verbose output
-"${GIT_TOOLS}/createGitHubRepo.py" --token "${token}" --url $GITHUB_URL -o rel "${repo}" -t ${team_id_rel} --description $description 
+# test repo exists in rel 
+out=$(curl -i -H "Authorization: token $SCMLUXADM_TOKEN" -X GET $GITHUB/api/v3/repos/rel/$repo | egrep '404 Not Found') || true
 
-# put a README.md inside
-git clone https://github.deutsche-boerse.de/rel/"${repo}".git
-cd "${repo}"
-echo "# $repo" >> README.md
-git add README.md
-git commit -m "first commit"
-git push -u origin master
+# if not, create it
+if [[ -n $out ]]; then
 
-cd ..
-rm -rf "${repo}"
+    # create repo in rel and add the default CFM team, add '-v' for verbose output
+    "${GIT_TOOLS}/createGitHubRepo.py" --token "${token}" --url $GITHUB_URL -o rel "${repo}" -t ${team_id_rel} --description $description 
+    
+    cd /tmp
+    # put a README.md inside
+    git clone https://github.deutsche-boerse.de/rel/"${repo}".git
+    cd "${repo}"
+    echo "# $repo" > README.md
+    git add README.md
+    git commit -m "first commit"
+    git push -u origin master
+    
+    cd ..
+    rm -rf "${repo}"
+else
+    echo Repo already exists in rel, skipping
+fi
 
-# fork to dev, add '-v' for verbose output
-"${GIT_TOOLS}/forkGitHubRepo.py" --sourceOrg rel --targetOrg dev "${repo}" --token "${token}"
+# test repo exists in dev
+out=$(curl -i -H "Authorization: token $SCMLUXADM_TOKEN" -X GET $GITHUB/api/v3/repos/dev/$repo | egrep '404 Not Found') || true
 
-echo Add topic on the repos
-$GIT_TOOLS_DIR/addTopic.bash -r "${repo}" -t ${topic} -o dev -u scmluxadm -k $SCMLUXADM_TOKEN
-$GIT_TOOLS_DIR/addTopic.bash -r "${repo}" -t ${topic} -o rel -u scmluxadm -k $SCMLUXADM_TOKEN
+# if not, fork it
+if [[ -n $out ]]; then
+    # fork to dev, add '-v' for verbose output
+    "${GIT_TOOLS}/forkGitHubRepo.py" --sourceOrg rel --targetOrg dev "${repo}" --token "${token}"
+else
+    echo Repo already exists in dev, skipping
+fi
 
-# add Team in dev
-teams_array=(${teams//,,/ })
-for t in ${teams_array[@]}; do
-    $GIT_TOOLS/addTeam.py "${repo}" dev $t 
+echo Add topics on repos
+topics_array=(${topics//,/ })
+for t in ${topics_array[@]}; do
+    $GIT_TOOLS_DIR/addTopic.bash -r "${repo}" -t ${t} -o dev -u scmluxadm -k $SCMLUXADM_TOKEN
+    $GIT_TOOLS_DIR/addTopic.bash -r "${repo}" -t ${t} -o rel -u scmluxadm -k $SCMLUXADM_TOKEN
 done
 
-# add Collaborator
-collaborators_array=(${collaborators//,/ })
-for t in ${collaborators_array[@]}; do
-    $GIT_TOOLS/addCollaborator.py "${repo}" dev $t 
-done
+## optional params
+# add Teams in dev
+if [[ -n $teams ]]; then
+    teams_array=(${teams//,,/ })
+    for t in ${teams_array[@]}; do
+        echo Add repo dev/$repo to team $t
+        $GIT_TOOLS/addTeam.py "${repo}" dev $t 
+    done
+else
+    echo No teams will be set
+fi
 
+# add Collaborators in dev
+if [[ -n $collaborators_dev ]]; then
+    collaborators_dev_array=(${collaborators_dev//,/ })
+    for t in ${collaborators_dev_array[@]}; do
+        echo Add collaborator $t on repo dev/$repo
+        $GIT_TOOLS/addCollaborator.py "${repo}" dev $t 
+    done
+else
+    echo No collaborators will be set
+fi
 
-
-
+## add Collaborators in rel
+#collaborators_rel_array=(${collaborators_rel//,/ })
+#for t in ${collaborators_rel_array[@]}; do
+#    $GIT_TOOLS/addCollaborator.py "${repo}" dev $t 
+#done
 
 exit_error() # {{{
 {
