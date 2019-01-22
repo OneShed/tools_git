a38b0ffa15e2015cda31e4787074fff51169b34e
vobadm <cfm@clearstream.com>
2019-01-22 12:09:45 +0100

    wip

--- team_repos_info.sh
+++ team_repos_info.sh
@@ -5,4 +5,4 @@ org=$2
 
 # e.g $0 441 dev # cs-ifs-devops
 
-/local/git/scm/tools_git/team_repos.sh $team_id | xargs -i -n 1 bash /local/git/scm/tools_git/repo_info.sh {} $org | tee /tmp/cs_ifs_devops.txt
+/local/git/scm/tools_git/team_repos.sh $team_id | xargs -i -n 1 bash /local/git/scm/tools_git/repo_info.sh {} $org
