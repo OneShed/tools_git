f01ffb85f2c2c358980c478887cea3f80306a7be
vobadm <cfm@clearstream.com>
2019-01-24 11:43:32 +0100

    fix wrong json parse

--- team_repos.sh
+++ team_repos.sh
@@ -5,4 +5,4 @@ set -u
 
 team_id=$1 # 441 dev/ifs-devops, 103 rel/SCM Luxembourg
 
-curl -L -s -u scmluxadm:$SCMLUXADM_TOKEN -X GET -H "Accept: application/vnd.github.hellcat-preview+json" -H "Content-Type: application/json" $GITHUB/api/v3/teams/$team_id/repos | egrep '\"name\":' | awk  '{print $2}' | sed 's/\"//g' | sed 's/,//g'
+curl -L -s -u scmluxadm:$SCMLUXADM_TOKEN -X GET -H "Accept: application/vnd.github.hellcat-preview+json" -H "Content-Type: application/json" $GITHUB/api/v3/teams/$team_id/repos | egrep '^\s{4}\"name\":' | awk  '{print $2}' | sed 's/\"//g' | sed 's/,//g'
