57af1a6a077776cde8716e6250fdcc183fdfb0c8
vobadm <cfm@clearstream.com>
2019-02-01 15:01:30 +0100

    new file:   lock_branch.sh

---
+++ lock_branch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+set -u
+set -x
+
+echo "add branch protection"
+
+repo=$1
+branch=$2
+
+user=scmluxadm
+
+curl -s -v -X PUT -H "accept: application/vnd.github.luke-cage-preview+json" -k --user scmluxadm:${SCMLUXADM_TOKEN} -i "https://github.deutsche-boerse.de/api/v3/repos/dev/$repo/branches/$branch/protection" --data '{
+  "required_status_checks": null,
+  "enforce_admins": false,
+  "required_pull_request_reviews": null, 
+  "restrictions": {
+    "users": [
+      "$user", 
+    ],
+    "teams": [
+      "cs-cfm-community"
+    ]
+  }
+}'
+
