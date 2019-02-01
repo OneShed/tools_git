4971224472d62066f496496bd26048bcda384320
vobadm <cfm@clearstream.com>
2019-02-01 15:16:09 +0100

    renamed:    create_pel_forks.sh -> modify_repos.sh

--- modify_repos.sh
+++ modify_repos.sh
@@ -4,9 +4,10 @@ set -e
 set -u
 
 cd /local/git/scm/build_scripts
+#cd /local/git/scm/pckg_list
 
 for repo in $(ls); do
     if [[ $repo != 'PckgElmnt.dtd' && ! ${repo} =~ ".sh" ]]; then
-        /local/git/scm/tools_git/lock_branch.sh "cs.$(echo ${repo,,})-build"  master
+#        /local/git/scm/tools_git/lock_branch.sh "cs.$(echo ${repo,,})-build"  master
     fi;
 done
