43ed3eb6faf66442838e85a321ff0ce96bdcf4c9
vobadm <cfm@clearstream.com>
2019-02-11 15:36:24 +0100

    new file:   digi 	modified:   push_master_to_remotes.sh

--- push_master_to_remotes.sh
+++ push_master_to_remotes.sh
@@ -6,6 +6,7 @@ set -u
 
 # in all repos, push master to origin
 
+echo Processing build_scripts directory 
 cd /local/git/scm/build_scripts
 URL=https://github.deutsche-boerse.de
 
@@ -20,4 +21,21 @@ for repo in $(ls | egrep -v OBSOLETE); do
         cd ..;
     fi;
 done
-echo All done
+
+echo
+echo Processing PEL directory
+
+cd /local/git/scm/pckg_list
+for repo in $(ls | egrep -v OBSOLETE); do
+    if [[ $repo != 'PckgElmnt.dtd' && ! ${repo} =~ ".sh" ]]; then
+        echo $repo;
+        cd $repo;
+
+        git push origin master
+        git push scm_repo master
+
+        cd ..;
+    fi;
+done
+
+echo $(basename $0): All done
