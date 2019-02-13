ba55df5e61da1a30790d7463b1d2e157f607f08e
vobadm <cfm@clearstream.com>
2019-02-13 10:11:33 +0100

    build script more verbose

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# CFM build script.
+# Can contain all build steps, or source from dev repo.
+
 set -e
 set -u
 #set -x
 
-## Build script here
+## Build steps here 
 
 echo $(basename $0): All done
